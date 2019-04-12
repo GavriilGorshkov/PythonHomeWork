fd49be8859fbcd52310782e38268ce6f4fe9e9b8
gga <211@smsfinance.ru>
2019-04-12 12:23:26 +0700

    HW1

---
+++ 1 lesson/2.py
@@ -0,0 +1,6 @@
+a = input('Введите число: ')
+
+def chetn(a):
+    print(int(int(a)/2) == int(a)/2)
+
+chetn(a)
---
+++ 1 lesson/3.py
@@ -0,0 +1,7 @@
+a = input('Введите первое число:')
+b = input('Введите второе число:')
+
+def chet_nechet(a,b):
+    print((int(int(a)/2) == int(a)/2) + (int(int(b)/2)) == int(b)/2)==1
+
+chet_nechet(a,b)
